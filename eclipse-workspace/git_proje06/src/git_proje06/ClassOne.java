7e84cbc1e39b7956f714557b45de212549eba6ad
Ibrahim <iakgobek27@gmail.com>
2020-07-10 13:19:23 -0500

    ilk commit master branchten

---
+++ eclipse-workspace/git_proje06/src/git_proje06/ClassOne.java
@@ -0,0 +1,13 @@
+package git_proje06;
+
+public class ClassOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("ilk commit local masterdan");
+		
+
+	}
+
+}
